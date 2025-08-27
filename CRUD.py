ff9c089c88a3bfd5e86ca065aa731b500448bcdf
Andy-Ops254 <ghostandy254@outlook.com>
2025-08-28 01:18:02 +0300

    Add team functionalities on cli app, remove id colums on crud

--- CRUD.py
+++ CRUD.py
@@ -1,14 +1,14 @@
 from models import Team, Player, Manager, session
 
 #Create ops
-def add_team (teams_id, name, city):
-    new_team = Team(teams_id=teams_id, name=name, city=city)
+def add_team (name, city):
+    new_team = Team( name=name, city=city)
     session.add(new_team)
     session.commit()
 
-def add_manager(managers_id, First_name, Last_name, email, Hiring_Date, Team_id):
+def add_manager( First_name, Last_name, email, Hiring_Date, Team_id):
     new_manager = Manager(
-        managers_id = managers_id,
+        # managers_id = managers_id,
         First_name = First_name,
         Last_name = Last_name,
         email = email,
@@ -18,9 +18,9 @@ def add_manager(managers_id, First_name, Last_name, email, Hiring_Date, Team_id)
     session.add(new_manager)
     session.commit()
 
-def add_player(players_id, first_name, last_name, age, height, position, jersey_number, Team_id ):
+def add_player(first_name, last_name, age, height, position, jersey_number, Team_id ):
     new_player = Player(
-        players_id = players_id,
+        # players_id = players_id,
         first_name = first_name,
         last_name = last_name,
         age = age,
--- main.py
+++ main.py
@@ -1,2 +1,47 @@
 import click
-from CRUD import 
+from CRUD import add_team, add_manager, add_player, show_players, show_teams, delete_manager,delete_player
+
+while True:
+    click.secho("WELCOME TO MY BASKETBALL PROGRAM MANAGER", fg='bright_white')
+    click.secho("Select an option to proceed", fg='yellow')
+    click.secho("1. Teams", fg="green")
+    click.secho("2. Manager", fg='green')
+    click.secho("3. Player", fg="green")
+
+    user_input = click.prompt("Seleect option", type=int)
+
+    if user_input == 1:
+        click.secho("1. Add new team", fg="yellow")
+        click.secho("2.View teams", fg='yellow')
+
+        team_option = click.prompt("Enter Team option", type=int)
+
+        if team_option == 1:
+            click.secho("Signing new Dawgs to the yard!...", fg="magenta")
+            name = click.prompt("Enter teame name")
+            city = click.prompt("State your hood")
+
+            try:
+                add_team(name, city)
+                click.secho(f"Welcome to the Gulag {name}")
+                click
+
+            except Exception as Dawgs:
+                click.secho(f"Error adding Team {Dawgs}", fg='red')
+
+        if team_option == 2:
+            click.secho("Team list", fg='magenta')
+            name = click.prompt("Team Name:")
+
+            try:
+                show_teams(name)
+                click.secho(f"{name}")
+
+            except Exception as err:
+                click.secho("Team does not exist")
+
+
+
+
+
+
